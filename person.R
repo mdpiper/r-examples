677f862f972d7d4707419fdaed50dc379500e023
Mark Piper <mark.piper@colorado.edu>
2025-10-14 14:49:49 -0600

    Add a class with examples in an Rmarkdown file

---
+++ person.R
@@ -0,0 +1,26 @@
+library(R6)
+
+# Person class
+# 
+# See `person.Rmd` for examples of usage.
+#
+Person <- R6Class("Person", list(
+
+  name = NULL,
+  age = NA,
+
+  initialize = function(name, age = NA) {
+    stopifnot(is.character(name), length(name) == 1)
+    stopifnot(is.numeric(age), length(age) == 1)
+    
+    self$name <- name
+    self$age <- age
+  },
+
+  print = function(...) {
+    cat("Person: \n")
+    cat("  Name: ", self$name, "\n", sep = "")
+    cat("  Age:  ", self$age, "\n", sep = "")
+    invisible(self)
+  }
+))
